0df04dafa834872821ccae26b8ff692c19166217
Elsa <116039323+elsafig@users.noreply.github.com>
2022-11-09 19:27:45 -0500

    comment outline

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,34 @@
+#given financial dataset called "budget_data.csv" with two columns "Date" and "Profit/Losses" 
 
+#variable for file 
+
+#analyze record to calculate  total number of months included in the dataset
+
+#variable for number_of_months
+
+#analyze record to calculate net total amount of "Profit/Losses" over the entire period 
+
+#variable for net_profitOrLoss
+
+#analyze record to calculate changes in "Profit/Losses" over the entire period, and then the average of those changes
+
+#variable for avg_profitOrLoss
+
+
+#analyze record to calculate greatest increase in profits (date and amount) over the entire period
+
+#variable for greatest_increaseProfit
+
+#analyze record to calculate greatest decrease in profits (date and amount) over the entire period
+
+#variable for greatest_decreaseProfit
+
+#output "Financial Analysis"
+#output "------------------"
+#output "Total Months: number_of_months"
+#output "Total: net_profitOrLoss"
+#output "Average Change: avg_profitOrLoss "
+#output "Greatest Increase in Profits: greatest_increaseProfit"
+#output "Greatest Decrease in Profits: greatest_decreaseProfit"
+
+#export text file with above results

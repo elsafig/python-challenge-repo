34fe7b93a3791590ffdbbcdb667c3a37c6a7ca4f
Elsa <116039323+elsafig@users.noreply.github.com>
2022-11-09 19:28:51 -0500

    main.py updated with comment outline

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,41 @@
+#given financial dataset called "election_data.csv" with three columns: "Voter ID", "County", and "Candidate"
 
+#variable for file  
+
+#analyze record to calculate total number of votes cast
+
+#variable for number_votesCast  
+
+#analyze record to calculate complete list of candidates who received votes
+
+#variable for list_candidatesWithVotes
+
+#analyze record to calculate percentage of votes each candidate won
+
+#variable for percentage_votesWon 
+
+#analyze record to calculate total number of votes each candidate won
+
+#variable for number_notesWon  
+
+#analyze record to calculate winner of the election based on popular vote
+
+#variable for winner 
+
+#output "Election Results"
+#output "-------------------------"
+#output "Total Votes: number_votesCast "
+#output "-------------------------"
+#output "name: percent (number) "
+#online exmaple output:
+#Charles Casper Stockham: 23.049% (85213)
+#output "name: percent (number) "
+#online exmaple output:
+#Diana DeGette: 73.812% (272892)
+#output "name: percent (number) "
+#online exmaple output: Raymon Anthony Doane: 3.139% (11606)
+#output"-------------------------
+#output "Winner: "
+#output"-------------------------
+
+#export text file with above results
